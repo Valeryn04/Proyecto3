dad4be68f557cbda8850a060760ae4adfaf9241a
Andres527 <85116170+Andres527@users.noreply.github.com>
2025-10-04 23:12:15 -0500

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<script>
-  import Login from "$lib/Login.svelte";
+<script lang="ts">
+    import LoginForm from "$lib/components/login-form.svelte";
 </script>
 
-<!-- Centrado con utilidades de Bootstrap -->
-<div>
-  <Login />
+<div class="flex h-screen w-full items-center justify-center px-4">
+  <LoginForm />
 </div>
+
